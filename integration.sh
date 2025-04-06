05b5dc1120f8b323c85674eda01fcbe8f6adff54
Sergio Romero <sergioromero48@outlook.com>
2025-04-05 20:38:21 -0500

    forked original project with a few fixes - integration.sh does not work yet

--- integration.sh
+++ integration.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pytest -rx tests_sitl/test_*.py -W ignore::DeprecationWarning --log-level=INFO
+python3 -m pytest -rx tests_sitl/test_*.py -W ignore::DeprecationWarning --log-level=INFO
